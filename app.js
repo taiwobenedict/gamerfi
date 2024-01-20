7409788e4b926e12fb50c6a03beba9c34cf55834
taiwobenedict <ajayitaiwotunta@gmail.com>
2024-01-20 04:54:45 +0100

    added smooth scroll functionality

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const themeBtn = document.getElementById('switch');
 const modeText = document.querySelector('.mode')
 
 
+// Smooth scrolling
+$("a").on("click", function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
+  $("html, body").animate({ scrollTop: $(href).offset().top  -100}, 500);
+});
+
 // Theme mode
 $("#switch").on("click", () => {
   if ($("#switch").prop("checked")) {
@@ -13,3 +20,4 @@ $("#switch").on("click", () => {
   }
 });
   
+
